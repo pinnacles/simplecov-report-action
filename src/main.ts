d6afe51c02becf70869f687cecdd5e142eacfe46
Yusuke Hatanaka <hatajoe@hey.com>
2022-12-07 14:20:01 +0000

    test

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,6 @@ async function run(): Promise<void> {
       core.warning('Cannot find the PR id.')
       return
     }
-    const pullRequestId = github.context.issue.number
-    core.debug(`pullRequestId ${pullRequestId}`)
 
     const headSha: string = core.getInput('headSha')
     core.debug(`headSha ${headSha}`)
@@ -36,6 +34,9 @@ async function run(): Promise<void> {
     const baseRefCoverageJson = require(path.resolve("./", baseRefCoveragePath))
       .metrics as CoverageReport
     core.debug(`read baseRefCoverageJson`)
+
+    const pullRequestId = github.context.issue.number
+    core.debug(`pullRequestId ${pullRequestId}`)
 
     await report(pullRequestId, headSha, headRefCoverageJson, baseRefCoverageJson)
   } catch (error) {
